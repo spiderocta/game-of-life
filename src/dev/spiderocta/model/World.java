7222a556e707a69ae62531816ff5a06e9edd3d0d
spiderocta <muhammadahmed@tutanota.com>
2023-07-21 16:15:21 +0300

    the world model

---
+++ src/dev/spiderocta/model/World.java
@@ -0,0 +1,22 @@
+package dev.spiderocta.model;
+
+public class World {
+	private int rows;
+	private int cols;
+	private boolean[][] grid;
+
+	public World(int rows, int cols) {
+		super();
+		this.rows = rows;
+		this.cols = cols;
+		this.grid = new boolean[rows][cols];
+	}
+
+	public boolean getCell(int row, int col) {
+		return grid[row][col];
+	}
+
+	public void setCell(int row, int col, boolean status) {
+		grid[row][col] = status;
+	}
+}
