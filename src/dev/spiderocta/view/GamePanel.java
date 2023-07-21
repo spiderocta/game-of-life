f3dcfd9ac75b4ce4ef732d7379958f2f8dc86e41
spiderocta <muhammadahmed@tutanota.com>
2023-07-21 16:07:49 +0300

    filling the cells

--- src/dev/spiderocta/view/GamePanel.java
+++ src/dev/spiderocta/view/GamePanel.java
@@ -38,24 +38,39 @@ public class GamePanel extends JPanel {
 		g2d.setColor(BACKGROUND_COLOR);
 		super.paintComponent(g2d);
 		g2d.fillRect(0, 0, width, height);
-		
+
 		drawGrid(g2d, width, height);
+		fillCell(g2d, 2, 4, true);
+		fillCell(g2d, 2, 4, false);
+		fillCell(g2d, 3, 5, true);
 	}
 
 	// method to draw the grid
 	private void drawGrid(Graphics g, int width, int height) {
 		g.setColor(GRID_COLOR);
 
-		// drawing the horizontal lines (X axis ) 
+		// drawing the horizontal lines (X axis )
 		for (int x = leftRightMargin; x <= width - leftRightMargin; x += CELLSIZE) {
 			g.drawLine(x, topBottomMargin, x, height - topBottomMargin);
 		}
-		
+
 		// drawing the crossing vertical lines (Y axis )
 		for (int y = topBottomMargin; y <= width - topBottomMargin; y += CELLSIZE) {
 			g.drawLine(leftRightMargin, y, width - leftRightMargin, y);
 		}
-		
-		
+
 	}
+	
+	// method to fill cells
+	private void fillCell(Graphics2D g, int row, int col, boolean status) {
+		Color color = status ? FOREROUND_COLOR : BACKGROUND_COLOR;
+		g.setColor(color);
+		
+		int x = leftRightMargin + col * CELLSIZE;
+		int y = topBottomMargin + row * CELLSIZE;
+		
+		g.fillRect(x + 2, y + 2, CELLSIZE - 2, CELLSIZE - 2);
+	}
+	
+	
 }
